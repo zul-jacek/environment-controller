cd6ff11190f60a81152221c062cc2bd16bac7641
Hans Herman <jacek255@gmx.de>
2025-03-16 20:18:40 +0100

    fix: fix 24h problem.

--- controller/control/time_control.py
+++ controller/control/time_control.py
@@ -8,28 +8,39 @@ class TimeControl():
         self.on_event = callback_func
         self.state = [False for _ in range(len(time_list))]
 
-    def str_to_time(self, string):
-        return Time(*map(int, string.split(':')))
+    def timestr_to_min(self, string):
+        values = string.split(':')
+        min = 60*int(values[0])+int(values[1])
+
+        return min
+    
+    def diff_in_min(self, t_1, t_2):
+        diff = t_2 - t_1
+        if diff < 0:
+            diff = 60*24 + diff
+        return diff
 
     def get_time_data(self):
 
         time_data = [
-            (name, pins, self.str_to_time(start), self.str_to_time(end))
+            (name, pins, self.timestr_to_min(start), self.timestr_to_min(end))
             for name, pins, (start, end) in self.time_list
         ]
 
         return time_data
 
-    def check_time_threshold(self, time_now, time_1, time_2):
-        if time_1 > time_2:
-            raise ValueError('begin must not be greater than end')
-        return time_1 <= time_now < time_2
+    def check_time_threshold(self, time_now, begin_time, end_time):
+        on_time = False
+        if (self.diff_in_min(time_now, end_time) <= self.diff_in_min(begin_time, end_time))and (self.diff_in_min(time_now, end_time) != 0):
+            on_time = True
+        
+        return on_time
 
     def check_time(self):
         while True:
             time.sleep(0.1)
             for i, (name, pin, begin_time, end_time) in enumerate(self.get_time_data()):
-                state_change = self.check_time_threshold(DateTime.now().time(),begin_time, end_time)
+                state_change = self.check_time_threshold(self.timestr_to_min(str(DateTime.now().time())),begin_time, end_time)
                 if (state_change == True) and (self.state[i] == False):
                     self.state[i] = True
                     self.on_event(name, pin, True)
@@ -45,7 +56,7 @@ if __name__ == '__main__':
     def callback(name, pin, state):
         print(name, pin, state)
 
-    time_list = [('led_1', [0, 1, 2, 3], ('23:50', '23:51'))]
+    time_list = [('led_1', [0, 1, 2, 3], ('23:00', '01:00'))]
     t_control = TimeControl(time_list=time_list, callback_func=callback)
     t_control.start_timer()
     while True:
